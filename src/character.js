cda5e95374f7dafc60c50cd47904c6977c2910d6
Olivier-Daire <daire.olivier@gmail.com>
2016-12-20 18:46:01 +0100

    :bug: Fix confusion between direction and vector direction

--- src/character.js
+++ src/character.js
@@ -9,25 +9,22 @@ var Character = function() {
 	this.theta = 0.75; // No idea why but it's in front of camera
 
 	this.followPath = function(path, direction){
-		//var radians = null;
-		//var tangent = new THREE.Vector3();
-		//var axis = new THREE.Vector3();
 		var right = new THREE.Vector3(0, 0, 1);
 		var left = new THREE.Vector3(0, 0, -1);
-		var currentDirection = direction === 'right' ? right : left;
+		var directionVector = direction === 'right' ? right : left;
 
 		this.fadeToAction('walk');
 		// http://stackoverflow.com/a/11181366
-		if (currentDirection === 'right') {
+		if (direction === 'right') {
 			if (this.theta <= 1) {
-				this.computeAngleAndDirection(path, currentDirection);
+				this.computeAngleAndDirection(path, directionVector);
 				this.theta += this.SPEED;
 			} else {
 				this.theta = 0;
 			}
 		} else {
 			if (this.theta >= 0) {
-				this.computeAngleAndDirection(path, currentDirection);
+				this.computeAngleAndDirection(path, directionVector);
 				this.theta -= this.SPEED;
 			} else {
 				this.theta = 1;
@@ -35,22 +32,24 @@ var Character = function() {
 		}
 	};
 
-	this.computeAngleAndDirection = function(path, currentDirection) {
+	this.computeAngleAndDirection = function(path, directionVector) {
 		var radians = null;
 		var tangent = new THREE.Vector3();
 		var axis = new THREE.Vector3();
 
 		this.model.position.copy( path.getPointAt(this.theta) );
 		tangent = path.getTangentAt(this.theta).normalize();
-		axis.crossVectors(currentDirection, tangent).normalize();
+		axis.crossVectors(directionVector, tangent).normalize();
 
-		radians = Math.acos(currentDirection.dot(tangent));
+		radians = Math.acos(directionVector.dot(tangent));
 
 		this.model.quaternion.setFromAxisAngle(axis, radians);
 	};
 
 	this.updateCharacter = function(characterPath, delta) {
 		var currentPosition = this.model.position;
+		//console.debug('X', this.nextPosition.x - currentPosition.x)
+		//console.debug('Z', this.nextPosition.z - currentPosition.z)
 
 		if(this.nextPosition.x - currentPosition.x >= this.SENSITIVITY_TO_TRIGGER_MOVE || this.nextPosition.z - currentPosition.z >= this.SENSITIVITY_TO_TRIGGER_MOVE) {
 				this.followPath(characterPath, 'right');
